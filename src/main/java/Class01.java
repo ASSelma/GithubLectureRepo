bb664632c787153554900d75dc270f8fe90ad644
Selma Asil <selmaaslan186@gmail.com>
2023-08-19 20:40:02 +0300

    Versiyon3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -8,5 +8,16 @@ public class Class01 {
         int x = 1;
         int y = 2;
         System.out.println(x+y);
+
+        //Versiyon 2
+
+        int z = 5;
+        System.out.println(x+y+z);
+
+
+        //Versiyon 3
+
+        System.out.println("Versiyon 3 için kodlar");
+
     }
 }
